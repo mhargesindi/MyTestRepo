d6a6d5fda9c561f697bb3a49e879ca68fb8eef95
Michael Harges <mharges@indiDx.com>
2017-07-21 15:02:33 -0700

    First change

---
+++ TestSolution/TestProject/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProject
+{
+   public class Person
+   {
+      public String LastName { get; set; }
+
+      public String FirstName { get; set; }
+
+   }  // Person
+}
